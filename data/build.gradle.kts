65c7d94ae3f59793dece1f14e10f7b4c0897144d
Sergey Kalinovsky <CriticalGnome@users.noreply.github.com>
2025-08-24 14:19:57 +0200

    Refactor dependencies and project structure (#5)
    MyCookBookBible-1: Refactor dependencies and project structure
    
    - Add Dagger Hilt for dependency injection.
    - Add JUnit 5 for testing.
    - Add KSP for annotation processing.
    - Add Room for database management.
    - Add MockK for mocking in tests.
    - Add SLF4J for logging.
    - Remove default example tests.
    - Remove default launcher icons.
    - Update Gradle version and dependencies.
    - Update AndroidManifest.xml to include the Application class.
    - Update Compose theme and UI elements.
    - Update XML resource files to remove trailing newlines.

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
+    alias(libs.plugins.ksp)
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.junit5)
 }
 
 android {
@@ -11,6 +14,7 @@ android {
         minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunnerArguments["runnerBuilder"] = "de.mannodermaus.junit5.AndroidJUnit5Builder"
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -32,12 +36,37 @@ android {
 }
 
 dependencies {
-
+    // Modules
     implementation(project(":domain"))
+    // Core
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    // Dagger
+    implementation(libs.dagger)
+    ksp(libs.dagger.compiler)
+    // Hilt
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
+    // Room
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+
+    // Junit 5
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.junit.jupiter.params)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+    // Coroutines
+    testImplementation(libs.kotlinx.coroutines.test)
+    // MockK
+    testImplementation(libs.mockk)
+    // Room
+    testImplementation(libs.room.testing)
+    // Core Android
+    androidTestImplementation(libs.runner)
+    // Junit 5 Android
+    androidTestImplementation(libs.junit.jupiter.api)
+    androidTestImplementation(libs.android.test.core)
+    androidTestRuntimeOnly(libs.android.test.runner)
+    // MockK Android
+    androidTestImplementation(libs.mockk.android)
 }
