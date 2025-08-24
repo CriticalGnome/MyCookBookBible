fb6b0eec7cf4b0d6d1522d1ab17519454cb2706d
Sergey Kalinovsky <lord.skiminok@gmail.com>
2025-08-24 14:17:14 +0200

    MyCookBookBible-1: Refactor dependencies and project structure
    - Add Dagger Hilt for dependency injection.
    - Add JUnit 5 for testing.
    - Add KSP for annotation processing.
    - Add Room for database management.
    - Add MockK for mocking in tests.
    - Add SLF4J for logging.
    - Remove default example tests.
    - Remove default launcher icons.
    - Update Gradle version and dependencies.
    - Update AndroidManifest.xml to include the Application class.
    - Update Compose theme and UI elements.
    - Update XML resource files to remove trailing newlines.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
+    alias(libs.plugins.ksp)
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.junit5)
 }
 
 android {
@@ -16,6 +19,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunnerArguments["runnerBuilder"] = "de.mannodermaus.junit5.AndroidJUnit5Builder"
     }
 
     buildTypes {
@@ -39,22 +43,43 @@ android {
 }
 
 dependencies {
-
+    // Modules
     implementation(project(":data"))
     implementation(project(":domain"))
+    // Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.material3)
+    implementation(libs.fragment.ktx)
+    // Compose
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    // Dagger
+    implementation(libs.dagger)
+    ksp(libs.dagger.compiler)
+    // Hilt
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
+    // Junit 5
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.junit.jupiter.params)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+    // Coroutines
+    testImplementation(libs.kotlinx.coroutines.test)
+    // MockK
+    testImplementation(libs.mockk)
+    // Core Android
+    androidTestImplementation(libs.runner)
+    // Junit 5 Android
+    androidTestImplementation(libs.junit.jupiter.api)
+    androidTestImplementation(libs.android.test.core)
+    androidTestRuntimeOnly(libs.android.test.runner)
+    // MockK Android
+    androidTestImplementation(libs.mockk.android)
 }
--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java-library")
+    alias(libs.plugins.ksp)
     alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 java {
@@ -10,4 +11,25 @@ kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
+}
+tasks.test {
+    useJUnitPlatform()
+}
+dependencies {
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    // Dagger
+    implementation(libs.dagger)
+    ksp(libs.dagger.compiler)
+    // Junit 5
+    testImplementation(libs.junit.jupiter.api)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+    // SLF4J
+    implementation(libs.slf4j.api)
+    implementation(libs.slf4j.simple)
+    testImplementation(libs.slf4j.simple)
+    // Coroutines
+    testImplementation(libs.kotlinx.coroutines.test)
+    // MockK
+    testImplementation(libs.mockk)
 }
