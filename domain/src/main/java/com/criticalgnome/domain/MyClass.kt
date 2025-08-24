5138fc518640e79d4cee3ace9c4d39a019282e58
Sergey Kalinovsky <lord.skiminok@gmail.com>
2025-08-24 12:32:42 +0200

    MyCookBookBible-2: Add data and domain modules
    - Add `data` and `domain` modules to the project.
    - Configure `data` module as an Android library and `domain` module as a Java/Kotlin library.
    - Set Java and Kotlin JVM target to 11 for both modules.
    - Add necessary dependencies to `data` and `app` modules.
    - Update project-level build files and IDE settings to include the new modules.
    - Add default `.gitignore`, Proguard rules, and test files for the `data` module.
    - Add default `.gitignore` and an empty class for the `domain` module.
    - Add inspection profiles for Compose previews.

---
+++ domain/src/main/java/com/criticalgnome/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.criticalgnome.domain
+
+class MyClass {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "My CookBook Bible"
 include(":app")
+include(":domain")
+include(":data")
